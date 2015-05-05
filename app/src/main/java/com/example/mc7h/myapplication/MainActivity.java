0beb34b523790da37b1f624615a5ea57b80b345b
Aaron.C.Chen <Aaron.C.Chen>
2015-05-05 17:04:51 +0800

    branch

--- app/src/main/java/com/example/mc7h/myapplication/MainActivity.java
+++ app/src/main/java/com/example/mc7h/myapplication/MainActivity.java
@@ -22,8 +22,6 @@ public class MainActivity extends ActionBarActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
-        //test Aaron wegfwergc
-        //test merge branch
     }
 
     @Override
