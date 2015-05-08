ef72a879588e19b02b8fb5b8089649837cf4dfec
Aaron.C.Chen <Aaron.C.Chen>
2015-05-08 11:53:58 +0800

    Training

---
+++ app/src/main/java/com/example/mc7h/myapplication/NewViewactivity.java
@@ -0,0 +1,16 @@
+package com.example.mc7h.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by Aaron.C.Chen on 2015/5/8.
+ */
+public class NewViewactivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
